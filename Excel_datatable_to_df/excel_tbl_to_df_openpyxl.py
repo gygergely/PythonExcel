61c3c1a8930bcb14055f61b20d1c87b785f7c626
gygergely <gyetvaigergely@gmail.com>
2020-08-08 11:29:00 +0200

    adding comments

--- Excel_datatable_to_df/excel_tbl_to_df_openpyxl.py
+++ Excel_datatable_to_df/excel_tbl_to_df_openpyxl.py
@@ -11,12 +11,16 @@ def load_data_table_to_df(xl_file_name, sheet_name, table_name):
     :return: pandas dataframe or in case of error None
     """
     try:
+        # Assign the Excel file to a variable
         wb = pyxl.load_workbook(xl_file_name)
+        # Get sheet
         ws = wb[sheet_name]
+        # Get table
         data_tbl = ws.tables[table_name]
-
+        # Get table range
         cells = ws[data_tbl.ref]
 
+        # Iterate through table range cells and create a list of lists
         cells_value = list()
 
         for cell in cells:
@@ -25,19 +29,15 @@ def load_data_table_to_df(xl_file_name, sheet_name, table_name):
                 row.append(cell[i].value)
 
             cells_value.append(row)
-        print(cells_value)
 
+        # Load the list to a dataframe
         df = pd.DataFrame(cells_value)
-
         # Grab the first row for the header
         df_header = df.iloc[0]
-
         # Get the data except the 1st row
         df = df[1:]
-
         # Set the 1st row as header
         df.columns = df_header
-
         # Reset df index
         df.reset_index(drop=True, inplace=True)
 
@@ -49,12 +49,11 @@ def load_data_table_to_df(xl_file_name, sheet_name, table_name):
         print(message)
 
 
-
 if __name__ == '__main__':
 
-    df_test1 = load_data_table_to_df(r"C:\Users\gerge\Desktop\data_table_test.xlsx", 'test_datatbl_1', 'tbl_test_3')
-    df_test2 = load_data_table_to_df(r"C:\Users\gerge\Desktop\data_table_test.xlsx", 'test_datatbl_2', 'tbl_test_1')
-    df_test3 = load_data_table_to_df(r"C:\Users\gerge\Desktop\data_table_test.xlsx", 'test_datatbl_2', 'tbl_test_2')
+    df_test1 = load_data_table_to_df('data_tbl_test.xlsx', 'test_datatbl_1', 'tbl_test_3')
+    df_test2 = load_data_table_to_df('data_tbl_test.xlsx', 'test_datatbl_2', 'tbl_test_1')
+    df_test3 = load_data_table_to_df('data_tbl_test.xlsx', 'test_datatbl_2', 'tbl_test_2')
 
     if df_test1 is not None:
         print(df_test1.head())
